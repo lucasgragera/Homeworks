8f6078281d3a69304875256672a07e082c9b95bc
Lucas Gragera <lucasgragera1@gmail.com>
2023-11-09 08:42:10 -0300

    Exceptions. Try-catch-finally block. Try-catch with resources. Thow. Throws. Logger.

--- src/exceptions/DisplaySizeException.java
+++ src/exceptions/DisplaySizeException.java
@@ -2,12 +2,8 @@ package exceptions;
 
 import java.util.Scanner;
 
-public class DisplaySizeException extends Exception{
-    public runTimeException(String message){
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        if (a<15)
-            throw new DisplaySizeException("Screen Size Exception");
+public class DisplaySizeException extends Exception {
+    public DisplaySizeException(String message) {
+        super(message);
     }
-}
 }
